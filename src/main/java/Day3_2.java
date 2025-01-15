a89e3f473d5c742cef0b9d3ee3d4964e2a983931
ellgrl <lzoik@mail.ru>
2025-01-15 15:58:33 +0300

    tasks

---
+++ src/main/java/Day3_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Day3_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        while (true) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            if (b == 0) {
+                break;
+            }
+            else {
+                double c = a / b;
+                System.out.println(c);
+            }
+        }
+    }
+
+}
