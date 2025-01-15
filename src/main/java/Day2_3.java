02081b12aef70dec2302c620f92a372cbdb59d24
ellgrl <lzoik@mail.ru>
2025-01-15 10:41:15 +0300

    tasks

---
+++ src/main/java/Day2_3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Day2_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a >= b){
+            System.out.println("Некорректный ввод");
+        }
+        int i = 0;
+        while (a > 0 && b > 0 && i < b) {
+            if ( i % 5 == 0 && i % 10 != 0 && i > a) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
