41497650526c7b69ea7cb39b9948d5176ff6223a
ellgrl <lzoik@mail.ru>
2025-01-15 16:12:37 +0300

    tasks

---
+++ src/main/java/Day3_3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Day3_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = 0;
+
+        while (counter < 5) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+
+            counter++;
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+            }
+        }
+    }
+
