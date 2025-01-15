8c33a31f75ffad5735f364721702320d9fb314d2
ellgrl <lzoik@mail.ru>
2025-01-15 14:15:59 +0300

    tasks

---
+++ src/main/java/Day3_1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Day3_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String city = sc.nextLine();
+            if (city.equals("Stop")) {
+                break;
+            }
+            switch (city){
+                case "Москва":
+                case "Ростов":
+                case "Владивосток":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Лондон":
+                case "Ливерпуль":
+                case "Манчестер":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+
+
+            }
+        }
+    }
+}
